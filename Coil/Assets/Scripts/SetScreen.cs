4d4c2d143539afc23d67960dbdb105e3216ff02e
Zen <yukimgabriel14@gmail.com>
2025-09-25 10:18:05 +0800

    1.5

--- Coil/Assets/Scripts/SetScreen.cs
+++ Coil/Assets/Scripts/SetScreen.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SetScreen : MonoBehaviour
 {
     [SerializeField] private GameObject nextLevelScreen;
+    [SerializeField] private GameObject pauseScreen;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,5 +22,53 @@ public class SetScreen : MonoBehaviour
         Time.timeScale = 0f;
         nextLevelScreen.SetActive(set);
     }
+
+    public void SetPauseScreen()
+    {
+        Time.timeScale = 0f;
+        pauseScreen.SetActive(true);
+    
+    }
+
+    public void UnSetPauseScreen()
+    {
+        Time.timeScale = 1f;
+        pauseScreen.SetActive(false);
+    }
+
+    public void GoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
+    }
+
+    public void GoLevel1()
+    {
+        SceneManager.LoadScene("SampleScene");
+        Time.timeScale = 1f;
+    }
+
+    public void GoLevels()
+    {
+        SceneManager.LoadScene("Levels");
+    }
+
+    public void GoExit()
+    {
+        Debug.Log("Quiting Game!");
+        Application.Quit();
+    }
+
+    public void GoTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+
+
+
+
+
+
 
 }
